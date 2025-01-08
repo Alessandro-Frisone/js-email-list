25b4f64ce0378e88c2cbb6a45b9d63865dd8960e
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-08 16:08:26 +0100

    feat: add email on HTML

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // DOM ELEMENTS
 const emailListElm = document.getElementById("email-list")
-const emailElm = document.getElementById("email")
 const generateElm = document.getElementById("generate-random")
 
 
 // FUNCTIONS
 function generate() {
     const emailArray = [];
-
+    emailListElm.innerHTML = ' ';
     for (let i = 0; i < 10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(function (response) {
             emailArray.push(response.data.response)
+            emailListElm.innerHTML += `<li>${response.data.response}<li>`
         })
     }
 console.log(emailArray)
