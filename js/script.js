1e35f4e0f4d28a88dfbb515ce8eb30a298a0f352
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-08 14:56:25 +0100

    feat: add button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 // DOM ELEMENTS
+const emailListElm = document.getElementById("email-list")
+const emailElm = document.getElementById("email")
+const generateElm = document.getElementById("generate-random")
+
 
 // FUNCTIONS
+function generate() {
+    const emailArray = [];
 
-const emailArray = [];
     for (let i = 0; i < 10; i++){
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(function (response) {
@@ -11,6 +16,13 @@ const emailArray = [];
     }
     
 console.log(emailArray)
+}
+
+// EVENTS
+generateElm.addEventListener('click', generate() {
+    
+})
+
 
 
 
